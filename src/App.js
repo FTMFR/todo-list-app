c66e1c6b43454fca3b558821d5d93512d1812650
FTMFR <fatemefarajzade018@gmail.com>
2023-09-25 18:16:26 +0330

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import AppContent from './components/AppContent';
+import AppHeader from './components/AppHeader';
+import PageTitle from './components/PageTitle';
+import styles from './styles/modules/app.module.scss';
 
-
-
-
-
-
-
-const App = () => {
+function App() {
     return (
         <>
             <div className="container">
-                TODO list
+                <PageTitle>TODO List</PageTitle>
+                <div className={styles.app__wrapper}>
+                    <AppHeader />
+                    <AppContent />
+                </div>
             </div>
+            <Toaster
+                position="bottom-right"
+                toastOptions={{
+                    style: {
+                        fontSize: '1.4rem',
+                    },
+                }}
+            />
         </>
-    )
-};
-
-
-
-
+    );
+}
 
 export default App;
